7dd18f077028ced64c80f92348ae8a63c05589ef
Nathan Amick <nathan.amick@lifeomic.com>
2023-03-23 10:37:11 -0400

    chore: Add TrackTile stories

---
+++ example/storybook/stories/TrackTile/AdvancedTrackerEditor.stories.tsx
@@ -0,0 +1,230 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+import { AdvancedTrackerEditor } from 'src/components/TrackTile/TrackerDetails/AdvancedTrackerEditor/AdvancedTrackerEditor';
+import { MockEnvironmentDecorator } from './util/MockEnvironmentDecorator';
+import {
+  Code,
+  Tracker,
+  TRACKER_PILLAR_CODE,
+  TRACKER_PILLAR_CODE_SYSTEM,
+} from 'src/components/TrackTile/services/TrackTileService';
+import { nutrition, activity, mindful, sleep } from './util/ontologies';
+import { withKnobs, select, boolean } from '@storybook/addon-knobs';
+// import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  HeaderButton,
+  HeaderButtons,
+  Item,
+} from 'react-navigation-header-buttons';
+import { notifySaveEditTrackerValue } from 'src/components/TrackTile/services/EmitterService';
+import { View } from 'react-native';
+
+const ontology = {} as any;
+
+const Stack = createNativeStackNavigator();
+
+storiesOf('AdvancedTrackerEditor', module)
+  .addDecorator(withKnobs)
+  .addDecorator(
+    MockEnvironmentDecorator({
+      ontology,
+    }),
+  )
+  .add('default', () => {
+    const pillarType = select(
+      'Pillar Type',
+      ['Healthy Plants', 'Activity', 'Mindfulness', 'Sleep'],
+      'Healthy Plants',
+    );
+
+    const fullDetails = boolean('Use full coding selection?', false);
+
+    Object.assign(ontology, nutrition);
+    let color = '#33C317';
+    let displayOne = '{{count}} Serving';
+    let stepAmount = 0.5;
+    let resourceType: Tracker['resourceType'] = 'Observation';
+    const codes: Code[] = [];
+
+    switch (pillarType) {
+      case 'Healthy Plants': {
+        color = '#33C317';
+        displayOne = '{{count}} Serving';
+        stepAmount = 0.5;
+        resourceType = 'Observation';
+        Object.assign(ontology, nutrition);
+        codes.push(
+          ...(fullDetails
+            ? [
+                {
+                  id: '14b53f07-d7cd-43f0-9780-75cd741b887a',
+                  code: 'd6aeee97-7600-447f-bfb3-e77e10131807',
+                  display: 'Grape',
+                  system: 'http://lifeomic.com/fhir/nutrition',
+                },
+                {
+                  id: '88b53f07-d5dc-49de-a61c-927f2b205aaa',
+                  code: 'ab23392e-744c-41e2-a1a2-d53b22d80b0e',
+                  display: 'Other Fruits',
+                  system: 'http://lifeomic.com/fhir/nutrition',
+                },
+                {
+                  id: '88b53f07-d5dc-49de-a61c-927f2b205aaa',
+                  code: 'ab23392e-744c-41e2-a1a2-d53b22d80b0e',
+                  display: 'Other Fruits',
+                  system: 'http://lifeomic.com/fhir/nutrition',
+                },
+              ]
+            : []),
+          {
+            code: '31b998e7-38d4-4ea0-8ec2-b5be9d8f1df6',
+            display: 'Fruits',
+            system: 'http://lifeomic.com/fhir/nutrition',
+          },
+        );
+        break;
+      }
+      case 'Activity': {
+        color = '#CD335E';
+        displayOne = '{{count}} Minute';
+        stepAmount = 5;
+        resourceType = 'Procedure';
+        Object.assign(ontology, activity);
+        if (fullDetails) {
+          codes.push(
+            {
+              id: '34b4febf-f342-b84c-d960-0d059bf0f5e8',
+              code: '002',
+              system: 'http://lifeomic.com/fhir/activity',
+              display: 'Running',
+            },
+            {
+              code: '008',
+              display: 'Cardio',
+              system: 'http://lifeomic.com/fhir/activity',
+            },
+          );
+        }
+        break;
+      }
+      case 'Mindfulness': {
+        color = '#EFC002';
+        displayOne = '{{count}} Minute';
+        stepAmount = 5;
+        resourceType = 'Procedure';
+        Object.assign(ontology, mindful);
+        codes.push({
+          code: '007',
+          system: 'http://lifeomic.com/fhir/activity',
+          display: 'Breathing',
+        });
+        break;
+      }
+      case 'Sleep': {
+        color = '#6956D0';
+        displayOne = '{{count}} Hour';
+        stepAmount = 15;
+        resourceType = 'Procedure';
+        Object.assign(ontology, sleep);
+        break;
+      }
+    }
+
+    codes.push({
+      code: TRACKER_PILLAR_CODE,
+      display: pillarType,
+      system: TRACKER_PILLAR_CODE_SYSTEM,
+    });
+
+    const ScreenComponent = () => (
+      <View style={{ backgroundColor: 'white' }}>
+        <AdvancedTrackerEditor
+          trackerValue={{
+            code: {
+              coding: [...codes],
+            },
+            createdDate: new Date(),
+            id: 'some-id',
+            value: resourceType === 'Procedure' ? stepAmount * 60 : 1,
+          }}
+          valuesContext={{
+            codeBelow: TRACKER_PILLAR_CODE,
+            system: TRACKER_PILLAR_CODE_SYSTEM,
+          }}
+          tracker={
+            {
+              code: TRACKER_PILLAR_CODE,
+              color,
+              system: TRACKER_PILLAR_CODE_SYSTEM,
+              resourceType,
+              units: [
+                {
+                  unit: 'unit-1',
+                  display: 'svgs',
+                  target: 5,
+                  default: true,
+                  displayOne,
+                  stepAmount,
+                },
+              ],
+            } as Partial<Tracker> as any
+          }
+        />
+      </View>
+    );
+
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Demo"
+            options={{
+              title: `Edit ${
+                resourceType === 'Observation'
+                  ? displayOne.replace('{{count}} ', '')
+                  : pillarType
+              }`,
+              headerRightContainerStyle: {
+                paddingRight: 16,
+              },
+              // eslint-disable-next-line react/display-name
+              headerRight: () => (
+                <HeaderButtons
+                  HeaderButtonComponent={(props) => (
+                    <HeaderButton {...props} color={'#02BFF1'} />
+                  )}
+                >
+                  <Item
+                    title="Save"
+                    onPress={async () => {
+                      const value = await new Promise(
+                        notifySaveEditTrackerValue,
+                      );
+
+                      alert(`Saved!\nNew Tracker Value: ${value?.value}`);
+                    }}
+                  />
+                </HeaderButtons>
+              ),
+              headerLeftContainerStyle: {
+                paddingLeft: 16,
+              },
+              // eslint-disable-next-line react/display-name
+              headerLeft: () => (
+                <HeaderButtons
+                  HeaderButtonComponent={(props) => (
+                    <HeaderButton {...props} color={'#02BFF1'} />
+                  )}
+                >
+                  <Item title="Cancel" onPress={() => alert('Go Back')} />
+                </HeaderButtons>
+              ),
+            }}
+            component={ScreenComponent}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  });
---
+++ example/storybook/stories/TrackTile/ManageTrackers.stories.tsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+import { ManageTrackers } from 'src/components/TrackTile/ManageTrackers/ManageTrackers';
+import { Tracker } from 'src/components/TrackTile/services/TrackTileService';
+import { MockEnvironmentDecorator } from './util/MockEnvironmentDecorator';
+import { action } from '@storybook/addon-actions';
+import { boolean } from '@storybook/addon-knobs';
+import {
+  Anchor,
+  Smartphone,
+  Camera,
+  Wifi,
+  Planet,
+  Aperture,
+} from '@lifeomic/chromicons-native';
+
+const defaultMetricTypes: Partial<Tracker>[] = [
+  {
+    id: '4',
+    name: 'Mindful',
+    icon: 'mindful',
+    metricId: '4',
+    target: 1,
+    unit: 't',
+  },
+  {
+    id: '3',
+    name: 'Protein',
+    icon: 'protein',
+    metricId: '3',
+    target: 1,
+    unit: 't',
+  },
+  {
+    id: '2',
+    name: 'Steps',
+    icon: 'running',
+    metricId: '2',
+    target: 1,
+    unit: 't',
+  },
+  {
+    id: '1',
+    name: 'Water',
+    icon: 'water',
+    metricId: '1',
+    target: 1,
+    unit: 't',
+  },
+  {
+    id: '5',
+    name: 'General',
+  },
+  {
+    id: '6',
+    name: 'General',
+  },
+];
+
+storiesOf('ManageTrackers', module)
+  .addDecorator(MockEnvironmentDecorator())
+  .add('default', () => (
+    <ManageTrackers
+      onOpenTracker={action('onOpenTracker')}
+      trackerRequestMeta={{
+        loading: false,
+        error: undefined,
+        trackers: defaultMetricTypes as Tracker[],
+        pillarTrackers: [],
+      }}
+      icons={
+        boolean('Use Custom Icons', false)
+          ? {
+              1: Anchor,
+              2: Smartphone,
+              3: Camera,
+              4: Planet,
+              5: Aperture,
+              6: Wifi,
+            }
+          : {}
+      }
+    />
+  ))
+  .add('loading', () => (
+    <ManageTrackers
+      onOpenTracker={action('onOpenTracker')}
+      trackerRequestMeta={{
+        loading: true,
+        error: undefined,
+        trackers: [],
+        pillarTrackers: [],
+      }}
+    />
+  ))
+  .add('error', () => (
+    <ManageTrackers
+      onOpenTracker={action('onOpenTracker')}
+      trackerRequestMeta={{
+        loading: false,
+        error: 'error',
+        trackers: [],
+        pillarTrackers: [],
+      }}
+    />
+  ))
+  .add('reorder error', () => (
+    <ManageTrackers
+      onOpenTracker={action('onOpenTracker')}
+      trackerRequestMeta={{
+        loading: false,
+        error: undefined,
+        trackers: defaultMetricTypes as Tracker[],
+        pillarTrackers: [],
+      }}
+      hasReorderError={true}
+    />
+  ));
