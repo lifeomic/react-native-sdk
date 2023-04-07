d4cf6e2197ff160e4a8fed37fcf7342fd84fa1ae
Bruce Glazier <bruce.glazier@lifeomic.com>
2023-04-07 13:42:11 -0400

    chore: migrate over translations

--- src/components/Wearables/WearableRow.tsx
+++ src/components/Wearables/WearableRow.tsx
@@ -317,7 +317,7 @@ export const WearableRow: FC<WearableRowProps> = (props) => {
         >
           <SwitchRow
             onValueChange={toggleBackgroundSync(wearable)}
-            accessibilityLabel={t('Toggle {{wearableName}} background sync', {
+            accessibilityLabel={t('toggle-wearable', {
               wearableName: t('background-sync', 'Background Sync'),
             })}
             disabled={disabled}
@@ -328,7 +328,7 @@ export const WearableRow: FC<WearableRowProps> = (props) => {
                 fontWeight: 'normal',
               },
             }}
-            title={t('background-sync-heading', 'Background Sync')}
+            title={t('background-sync', 'Background Sync')}
             {...switchProps}
           />
           {backgroundSyncEnabled && (
