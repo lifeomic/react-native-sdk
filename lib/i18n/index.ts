a66e86ee3a60d515811f25f8f1fc446d499416db
Bruce Glazier <bruce.glazier@lifeomic.com>
2023-05-10 11:33:01 -0400

    chore: Cleanup discussions screen

--- lib/i18n/index.ts
+++ lib/i18n/index.ts
@@ -1,5 +1,6 @@
 import 'intl-pluralrules';
 import i18next, { t } from 'i18next';
+import resourcesToBackend from 'i18next-resources-to-backend';
 import { initReactI18next } from 'react-i18next';
 export { Trans } from 'react-i18next';
 
@@ -12,18 +13,40 @@ declare module 'i18next' {
 
 // Skeleton instance will enable building i18n ready components
 export function configure() {
-  return i18next.use(initReactI18next).init({
-    fallbackLng: 'en',
-    debug: false && __DEV__ && process.env.NODE_ENV !== 'test', // Remove `false` to debug locally
+  return i18next
+    .use(initReactI18next)
+    .use(
+      resourcesToBackend((language: string) => {
+        switch (language) {
+          case 'en':
+            return import('./locales/en.json');
+          case 'es':
+            return import('./locales/es.json');
+          case 'tr':
+            return import('./locales/tr.json');
+          case 'fr':
+            return import('./locales/fr.json');
+          case 'ar':
+            return import('./locales/ar.json');
+          case 'pt':
+            return import('./locales/pt.json');
+          default:
+            break;
+        }
+      }),
+    )
+    .init({
+      fallbackLng: 'en',
+      debug: false && __DEV__ && process.env.NODE_ENV !== 'test', // Remove `false` to debug locally
 
-    react: {
-      useSuspense: true,
-    },
+      react: {
+        useSuspense: true,
+      },
 
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+      interpolation: {
+        escapeValue: false,
+      },
+    });
 }
 
 // There are some load order issues in the test suite, and this is needed to ensure that
