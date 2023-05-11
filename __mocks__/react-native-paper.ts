6e4e787ae276e9cd98bcf478079f053053805527
Nathan Amick <nathan.amick@lifeomic.com>
2023-05-11 15:40:13 -0400

    fix: Update react-native-paper mock

--- __mocks__/react-native-paper.ts
+++ __mocks__/react-native-paper.ts
@@ -7,3 +7,9 @@ export const useTheme = jest.fn().mockReturnValue({
   ...DefaultTheme,
   ...baseDefaultTheme,
 });
+
+export const Appbar = {
+  Header: jest.fn(),
+  BackAction: jest.fn(),
+  Content: jest.fn(),
+};
