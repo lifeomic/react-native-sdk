dc71f93321c246d0a56648a9190d18981f4d0003
Bruce Glazier <bruce.glazier@lifeomic.com>
2023-07-27 13:39:42 -0400

    feat: Allow inject item and absolute positioning

--- src/components/LogoHeader.tsx
+++ src/components/LogoHeader.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-
 import { View, Image, ImageSourcePropType, Platform } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { createStyles } from './BrandConfigProvider';
 import { useStyles } from '../hooks/useStyles';
 
+type Percentage = `${number}%`;
+
 interface Props {
   imageSource: ImageSourcePropType;
+  item?: JSX.Element;
+  alignImage?: Percentage;
+  alignItem?: Percentage;
 }
 
-export function LogoHeader({ imageSource }: Props) {
+export function LogoHeader({
+  imageSource,
+  item,
+  alignImage,
+  alignItem,
+}: Props) {
   const { styles } = useStyles(defaultStyles);
   const androidFix = Platform.OS === 'android' ? { marginTop: 12 } : {};
 
@@ -19,7 +28,18 @@ export function LogoHeader({ imageSource }: Props) {
         edges={['left', 'right', 'top']}
         style={[androidFix, styles.safeAreaView]}
       >
-        <Image source={imageSource} style={styles.image} />
+        <View style={styles.contentView}>
+          <Image
+            source={imageSource}
+            style={[
+              {
+                left: alignImage,
+              },
+              styles.image,
+            ]}
+          />
+          <View style={[{ left: alignItem }, styles.itemView]}>{item}</View>
+        </View>
       </SafeAreaView>
     </View>
   );
@@ -27,8 +47,10 @@ export function LogoHeader({ imageSource }: Props) {
 
 const defaultStyles = createStyles('LogoHeader', () => ({
   view: {},
-  safeAreaView: { alignItems: 'center' },
-  image: {},
+  safeAreaView: {},
+  image: { position: 'absolute' },
+  itemView: { position: 'absolute' },
+  contentView: { alignItems: 'center', minHeight: '20%', overflow: 'visible' },
 }));
 
 declare module '@styles' {
