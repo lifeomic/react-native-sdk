0b52e2b37fe495bc37d4e8e33ee4553a56230748
Swain Molster <swain.molster@lifeomic.com>
2023-08-21 13:24:07 -0400

    chore: add core of one-query integration

--- jest.config.js
+++ jest.config.js
@@ -40,6 +40,6 @@ module.exports = {
     },
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?@?react-native|query-string|decode-uri-component|split-on-first|filter-obj|victory)',
+    'node_modules/(?!(jest-)?@?react-native|query-string|decode-uri-component|split-on-first|filter-obj|victory|@lifeomic/one-query)',
   ]
 };
---
+++ src/types/rest-types.ts
@@ -0,0 +1,8 @@
+/**
+ * This type definition describes known endpoints in our REST API.
+ *
+ * It is structured in the format specified by [one-query](https://github.com/lifeomic/one-query).
+ */
+export type RestAPIEndpoints = {
+  // TODO: see follow-up PR.
+};
