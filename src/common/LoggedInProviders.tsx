0820aafb05535f0229082abbeb51aec253712e43
Bruce Glazier <bruce.glazier@lifeomic.com>
2023-09-08 08:56:48 -0400

    fix: Separate LoggedIn Providers from Root

--- src/common/LoggedInProviders.tsx
+++ src/common/LoggedInProviders.tsx
@@ -3,17 +3,43 @@ import React from 'react';
 import { ActiveAccountContextProvider } from '../hooks/useActiveAccount';
 import { NotificationsManagerProvider } from '../hooks/useNotificationManager';
 import { UnreadMessagesContextProvider } from '../hooks/useUnreadMessages';
+import { ActiveProjectContextProvider } from '../hooks/useActiveProject';
+import Toast from 'react-native-toast-message';
+import { TrackTileProvider } from '../components/TrackTile/TrackTileProvider';
+import { WearableLifecycleProvider } from '../components/Wearables/WearableLifecycleProvider';
+import { CreateEditPostModal } from '../components/Circles/CreateEditPostModal';
+import { PushNotificationsProvider } from '../hooks/usePushNotifications';
+import { CircleTileContextProvider } from '../hooks/Circles/useActiveCircleTile';
+import { OnboardingCourseContextProvider } from '../hooks/useOnboardingCourse';
+import { useDeveloperConfig } from '../hooks/useDeveloperConfig';
 
 export const LoggedInProviders = ({
   children,
 }: {
   children?: React.ReactNode;
 }) => {
+  const { pushNotificationsConfig } = useDeveloperConfig();
   return (
-    <NotificationsManagerProvider>
-      <UnreadMessagesContextProvider>
-        <ActiveAccountContextProvider>{children}</ActiveAccountContextProvider>
-      </UnreadMessagesContextProvider>
-    </NotificationsManagerProvider>
+    <ActiveAccountContextProvider>
+      <ActiveProjectContextProvider>
+        <TrackTileProvider>
+          <WearableLifecycleProvider>
+            <CircleTileContextProvider>
+              <OnboardingCourseContextProvider>
+                <PushNotificationsProvider config={pushNotificationsConfig}>
+                  <NotificationsManagerProvider>
+                    <UnreadMessagesContextProvider>
+                      {children}
+                    </UnreadMessagesContextProvider>
+                  </NotificationsManagerProvider>
+                </PushNotificationsProvider>
+                <CreateEditPostModal />
+                <Toast />
+              </OnboardingCourseContextProvider>
+            </CircleTileContextProvider>
+          </WearableLifecycleProvider>
+        </TrackTileProvider>
+      </ActiveProjectContextProvider>
+    </ActiveAccountContextProvider>
   );
 };
