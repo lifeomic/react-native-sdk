ff8f552f0a89f5871a15c9d47e4e95876bc82c9e
Aymane Nadi <aymane.nadi@lifeomic.com>
2023-09-29 15:32:29 -0400

    fix: export useGraphQLClient

--- example/AppDemo.tsx
+++ example/AppDemo.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { authConfig, baseURL } from './storybook/helpers/oauthConfig';
-import { DeveloperConfigProvider, RootProviders, RootStack } from '../src';
+import {
+  DeveloperConfigProvider,
+  HomeStackParamList,
+  RootProviders,
+  RootStack,
+} from '../src';
 import { FhirExampleScreen } from './src/screens/FhirExampleScreen';
+import { View, Text } from 'react-native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 
 if (__DEV__) {
   import('./reactotron').then(() => console.log('Reactotron Configured'));
 }
+
+type CustomHomeStackParamsList = HomeStackParamList & {
+  CustomHomeScreen: undefined;
+};
+type UserDetailsScreenRouteProp = RouteProp<
+  CustomHomeStackParamsList,
+  'CustomHomeScreen'
+>;
+
+type CustomHomeScreenNavigationProp = NativeStackNavigationProp<
+  CustomHomeStackParamsList,
+  'CustomHomeScreen'
+>;
+
+const CustomHomeScreen = () => {
+  const navigation = useNavigation<CustomHomeScreenNavigationProp>();
+  const route = useRoute<UserDetailsScreenRouteProp>();
+
+  return (
+    <View>
+      <Text>Custom Home Screen</Text>
+    </View>
+  );
+};
 
 function App() {
   return (
@@ -19,6 +51,13 @@ function App() {
           primaryColor: '#fb5607',
         },
         apiBaseURL: baseURL,
+        getAdditionalHomeScreens: (HomeStack) => [
+          <HomeStack.Screen
+            name="Home"
+            component={CustomHomeScreen}
+            key={'CustomHomeScreen'}
+          />,
+        ],
       }}
     >
       <RootProviders authConfig={authConfig}>
