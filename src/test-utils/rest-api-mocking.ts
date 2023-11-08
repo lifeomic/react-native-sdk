de853026f639b2995e7edf7b3419824bd3c0f132
Swain Molster <swain.molster@lifeomic.com>
2023-11-08 18:02:55 -0500

    fixup usage

--- src/test-utils/rest-api-mocking.ts
+++ src/test-utils/rest-api-mocking.ts
@@ -1,6 +1,11 @@
 import { createAPIMockingUtility } from '@lifeomic/one-query/test-utils';
 import { RestAPIEndpoints } from '../types/rest-types';
+import { setupServer } from 'msw/node';
+
+const server = setupServer();
+server.listen({ onUnhandledRequest: 'error' });
 
 export const createRestAPIMock = createAPIMockingUtility<RestAPIEndpoints>({
+  server,
   baseUrl: 'https://api.us.lifeomic.com',
 });
