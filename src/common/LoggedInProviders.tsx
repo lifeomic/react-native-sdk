8f33139b67ba1eab0b318f4d40c59e2c3b756a93
Swain Molster <swain.molster@lifeomic.com>
2023-11-15 14:04:29 -0500

    feat!: raise logged-out UI

--- src/common/LoggedInProviders.tsx
+++ src/common/LoggedInProviders.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { ActiveAccountContextProvider } from '../hooks/useActiveAccount';
 import { NotificationsManagerProvider } from '../hooks/useNotificationManager';
 import { ActiveProjectContextProvider } from '../hooks/useActiveProject';
-import Toast from 'react-native-toast-message';
 import { TrackTileProvider } from '../components/TrackTile/TrackTileProvider';
 import { WearableLifecycleProvider } from '../components/Wearables/WearableLifecycleProvider';
 import { CreateEditPostModal } from '../components/Circles/CreateEditPostModal';
@@ -11,13 +10,45 @@ import { PushNotificationsProvider } from '../hooks/usePushNotifications';
 import { CircleTileContextProvider } from '../hooks/Circles/useActiveCircleTile';
 import { OnboardingCourseContextProvider } from '../hooks/useOnboardingCourse';
 import { useDeveloperConfig } from '../hooks/useDeveloperConfig';
+import { useAuth } from '../hooks';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NotLoggedInRootParamList } from '../navigators';
+import { LoginScreen } from '../screens';
+import { ActivityIndicatorView } from '../components';
+import { t } from 'i18next';
 
-export const LoggedInProviders = ({
-  children,
-}: {
+export type LoggedInProvidersProps = {
   children?: React.ReactNode;
-}) => {
+};
+
+const LoggedOutStack = createNativeStackNavigator<NotLoggedInRootParamList>();
+
+export const LoggedInProviders = ({ children }: LoggedInProvidersProps) => {
+  const auth = useAuth();
   const { pushNotificationsConfig } = useDeveloperConfig();
+
+  if (!auth.isLoggedIn && auth.loading) {
+    return (
+      <ActivityIndicatorView
+        message={t('root-stack-waiting-for-auth', 'Waiting for authorization')}
+      />
+    );
+  }
+
+  if (!auth.isLoggedIn) {
+    return (
+      <LoggedOutStack.Navigator>
+        <LoggedOutStack.Group>
+          <LoggedOutStack.Screen
+            name="screens/LoginScreen"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+        </LoggedOutStack.Group>
+      </LoggedOutStack.Navigator>
+    );
+  }
+
   return (
     <ActiveAccountContextProvider>
       <ActiveProjectContextProvider>
@@ -31,7 +62,6 @@ export const LoggedInProviders = ({
                   </NotificationsManagerProvider>
                 </PushNotificationsProvider>
                 <CreateEditPostModal />
-                <Toast />
               </OnboardingCourseContextProvider>
             </CircleTileContextProvider>
           </WearableLifecycleProvider>
--- src/navigators/RootStack.tsx
+++ src/navigators/RootStack.tsx
@@ -1,36 +1,9 @@
 import React from 'react';
-import { t } from 'i18next';
-import { ActivityIndicatorView } from '../components/ActivityIndicatorView';
-import { useAuth } from '../hooks';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { LoginScreen } from '../screens/LoginScreen';
-import { NotLoggedInRootParamList } from './types';
 import { LoggedInStack } from './LoggedInStack';
 
+/**
+ * @deprecated use LoggedInStack instead
+ */
 export function RootStack() {
-  const { isLoggedIn, loading: loadingAuth } = useAuth();
-  if (!isLoggedIn && loadingAuth) {
-    return (
-      <ActivityIndicatorView
-        message={t('root-stack-waiting-for-auth', 'Waiting for authorization')}
-      />
-    );
-  }
-
-  if (isLoggedIn) {
-    return <LoggedInStack />;
-  }
-
-  const Stack = createNativeStackNavigator<NotLoggedInRootParamList>();
-  return (
-    <Stack.Navigator>
-      <Stack.Group>
-        <Stack.Screen
-          name="screens/LoginScreen"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Group>
-    </Stack.Navigator>
-  );
+  return <LoggedInStack />;
 }
