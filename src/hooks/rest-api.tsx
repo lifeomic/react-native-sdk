a358183a5974906f364dcf13d8b13319ef11b10c
Matt Nicolls <matt.nicolls@lifeomic.com>
2024-02-15 17:49:15 +0100

    fix one type error test

--- src/hooks/rest-api.tsx
+++ src/hooks/rest-api.tsx
@@ -4,21 +4,46 @@ import {
 } from '@lifeomic/react-client';
 import { useHttpClient } from './useHttpClient';
 import { RestAPIEndpoints } from '../types/rest-types';
-import { APIQueryHooks } from '@lifeomic/one-query';
+import { APIQueryHooks, RequestPayloadOf } from '@lifeomic/one-query';
+import {
+  UseQueryOptions,
+  UseQueryResult,
+} from '@tanstack/react-query/build/lib/types';
+import { AxiosRequestConfig } from 'axios';
 
-const hooks: APIQueryHooks<RestAPIEndpoints & ClientRestAPIEndpoints> =
-  createRestAPIHooks<RestAPIEndpoints>({
-    name: 'lifeomic-react-native-sdk',
-    client: () => {
-      // This function is explicitly documented as being hooks-compatible.
-      //
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      const { httpClient } = useHttpClient();
-      return httpClient;
-    },
-  });
+type Endpoints = RestAPIEndpoints & ClientRestAPIEndpoints;
 
-export const useRestQuery = hooks.useAPIQuery;
+declare type RestrictedUseQueryOptions<
+  Response,
+  TError = unknown,
+  Data = Response,
+> = Omit<UseQueryOptions<Response, TError, Data>, 'queryKey' | 'queryFn'> & {
+  axios?: AxiosRequestConfig;
+};
+
+const hooks: APIQueryHooks<Endpoints> = createRestAPIHooks<Endpoints>({
+  name: 'lifeomic-react-native-sdk',
+  client: () => {
+    // This function is explicitly documented as being hooks-compatible.
+    //
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const { httpClient } = useHttpClient();
+    return httpClient;
+  },
+});
+
+export const useRestQuery: <
+  Route extends keyof Endpoints & string,
+  Data = Endpoints[Route]['Response'],
+>(
+  route: Route,
+  payload: RequestPayloadOf<Endpoints, Route>,
+  options?: RestrictedUseQueryOptions<
+    Endpoints[Route]['Response'],
+    unknown,
+    Data
+  >,
+) => UseQueryResult<Data> = hooks.useAPIQuery;
 
 export const useInfiniteRestQuery = hooks.useInfiniteAPIQuery;
 
